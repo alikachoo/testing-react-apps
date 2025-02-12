186215c8f4880d199609748153d8288e481962d5
Alika Choo <alikachoo5@gmail.com>
2025-02-12 16:41:02 +0800

    complete all of ex4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,32 +5,41 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot';
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password())
+  }
+})
 
 test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
-  let submittedData = {}
-  const handleSubmit = data => submittedData = data;
+
+  const handleSubmit = jest.fn();
 
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
   render(<Login onSubmit={handleSubmit} />)
 
+  screen.debug();
+
   // 🐨 get the username and password fields via `getByLabelText`
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
-  const username = screen.getByLabelText('Username');
-  const password = screen.getByLabelText(/password/i);
+  const {username, password} = buildLoginForm();
+  console.log(username, password)
 
-  console.log(username)
-
-  await userEvent.type(username, "Hello")
-  await userEvent.type(password, 'password')
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 click on the button with the text "Submit"
   const submitButton = screen.getByRole('button', {name: /submit/i});
   submitButton.click()
 
   // assert that submittedData is correct
-  expect(submittedData).toEqual({username: 'Hello', password: 'password'})
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 })
 
 /*
